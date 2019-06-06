cb1264c18bcb592f05352bad50e47bc6f1116ace
dzharkov <dmytro.zharkov@zalando.de>
2019-06-06 11:04:47 +0200

    Add event emitter

--- src/app/superheroes/components/superheroes-list/superheroes-list.component.ts
+++ src/app/superheroes/components/superheroes-list/superheroes-list.component.ts
@@ -6,6 +6,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 import {MatSnackBar} from '@angular/material';
 import {environment} from '../../../../environments/environment';
 import Web3 from 'web3';
+import {WebSocketsService} from '../../../common/services/web-sockets.service';
 
 @Component({
   selector: 'app-superheroes-list',
@@ -36,31 +37,29 @@ export class SuperheroesListComponent {
   public superheroes: Superhero[] = [];
   public currentUniverse = '';
 
-  constructor(private superheroesService: SuperheroesService, private route: ActivatedRoute, private snackBar: MatSnackBar) {
+  constructor(private superheroesService: SuperheroesService, private route: ActivatedRoute, private snackBar: MatSnackBar, private websoket: WebSocketsService) {
     this.superheroes = this.route.snapshot.data.RPCData;
 
-    const web3 = new Web3(
-      new Web3.providers.WebsocketProvider(environment.RPCWSSProvider)
-    );
-    const instance = new web3.eth.Contract(environment.ABI.abi, environment.ABI.networks[environment.networkID].address);
-    instance.events.NewSuperhero()
-      .on('data', (event) => {
-        const {id, name, avatar, category, description} = event.returnValues;
-        const superHero = {
-          id,
-          name,
-          category,
-          avatar,
-          description,
-          isOpen: true
-        };
+    this.websoket.socketOpened.subscribe(() => {
+      this.websoket.subscribe('NewSuperhero');
+    });
 
-        setTimeout(() => {
-          superHero.isOpen = false;
-        }, 1000);
-        this.superheroes.unshift(superHero);
-      })
-      .on('error', console.error);
+    this.websoket.listen((data) => {
+      const {id, name, avatar, category, description} = data;
+      const superHero = {
+        id,
+        name,
+        category,
+        avatar,
+        description,
+        isOpen: true
+      };
+
+      setTimeout(() => {
+        superHero.isOpen = false;
+      }, 1000);
+      this.superheroes.unshift(superHero);
+    });
   }
 
   setUniverse(universe: string) {
